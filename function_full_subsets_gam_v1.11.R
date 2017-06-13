eb4ec6520b5e8ba383f32b0dd03f7044dd0615e3
beckyfisher <r.fisher@aims.gov.au>
2017-06-13 14:32:24 +0800

    re-arranged argument order.

--- function_full_subsets_gam_v1.11.R
+++ function_full_subsets_gam_v1.11.R
@@ -1,12 +1,22 @@
-
-###### fuction to fit full subsets gam #########################################
-full.subsets.gam=function(use.dat,test.fit,pred.vars.cont,pred.vars.fact=NA,
-                         cyclic.vars=NA,linear.vars=NA,size=3,cov.cutoff=0.28,k=5,parallel=F,n.cores=4,
-                         weight.vec=rep(1,nrow(use.dat)),null.terms="",bs.arg="'cr'",
-                         smooth.interactions=pred.vars.fact,
-                         factor.interactions=F, max.models=500,r2.type="r2.lm.est",
-                         report.unique.r2=F){
-
+full.subsets.gam=function(use.dat,
+                          test.fit,
+                          pred.vars.cont,
+                          pred.vars.fact=NA,
+                          cyclic.vars=NA,
+                          linear.vars=NA,
+                          smooth.interactions=pred.vars.fact,
+                          factor.interactions=F,
+                          cov.cutoff=0.28,
+                          size=3,
+                          k=5,
+                          bs.arg="'cr'",
+                          null.terms="",
+                          weight.vec=rep(1,nrow(use.dat)),
+                          max.models=500,
+                          parallel=F,
+                          n.cores=4,
+                          r2.type="r2.lm.est",
+                          report.unique.r2=F){
 
   # make an "intercept" term for the null model
   use.dat$intercept=1
