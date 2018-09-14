98ac222734481396401dc73b0a46bd58407acc66
beckyfisher <r.fisher@aims.gov.au>
2018-09-14 15:45:29 +0800

    New file to run package build.

---
+++ build_package_FFSgam.R
@@ -0,0 +1,23 @@
+
+
+# This script includes the basic steps required to create and
+#    build the FFSgam package
+
+library(devtools)
+library(roxygen2)
+library(knitr)
+library(R.rsp)
+
+setwd("C:/Users/rfisher/OneDrive - Australian Institute of Marine Science/Documents/AIMS/EcologicalRiskModelling/Full_subsets_methods/FSSgam/FSSgam")
+# copy function files over to FFSgam/R
+
+devtools::document()
+
+devtools::use_package("doSNOW")
+devtools::use_package("MuMIn")
+devtools::use_package("gamm4")
+devtools::use_package("mgcv")
+devtools::use_package("nnet")
+devtools::build()
+
+
