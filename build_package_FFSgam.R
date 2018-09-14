878e8c9d1f21363adb822113ce1a3830f105215c
beckyfisher <r.fisher@aims.gov.au>
2018-09-14 16:27:14 +0800

    Edits to repro path for pckage build.

--- build_package_FFSgam.R
+++ build_package_FFSgam.R
@@ -8,7 +8,7 @@ library(roxygen2)
 library(knitr)
 library(R.rsp)
 
-setwd("C:/Users/rfisher/OneDrive - Australian Institute of Marine Science/Documents/AIMS/EcologicalRiskModelling/Full_subsets_methods/FSSgam/FSSgam")
+setwd("C:/Users/rfisher/OneDrive - Australian Institute of Marine Science/Documents/AIMS/EcologicalRiskModelling/Full_subsets_methods/FSSgam_package")
 # copy function files over to FFSgam/R
 
 devtools::document()
@@ -20,4 +20,4 @@ devtools::use_package("mgcv")
 devtools::use_package("nnet")
 devtools::build()
 
-
+#devtools::install_github("beckyfisher/FSSgam_package")
