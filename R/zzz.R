0b49227d55735cc44e08d2629382757e371f06d2
Rebecca Fisher <r.fisher@aims.gov.au>
2026-05-01 14:45:03 +0800

    Fix pkgdown build and citation for docs-only site

---
+++ R/zzz.R
@@ -0,0 +1,8 @@
+#' Docs-only package used for building the pkgdown site
+#'
+#' This package contains no exported functions. It exists solely so that
+#' pkgdown can load the package namespace when building the documentation site.
+#'
+#' @keywords internal
+#' @noRd
+NULL
